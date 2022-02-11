19fc6756184dd4b772a7b4839f7a7c05d0f11b92
Fernando dos Santos <nando4803@gmail.com>
2022-02-11 22:32:27 +0100

    chore: export file

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from './Button'
+export { default as Game } from './Game'
+export { default as Container } from './Container'
+export { default as Heading } from './Heading'
+export { default as InputField } from './InputField'
+export { default as Modal } from './Modal'
+export { default as ThemeProvider } from './ThemeProvider'
