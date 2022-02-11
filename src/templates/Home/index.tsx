5cfd70b49ab8db0a63703be418c3db0ae3b7b35f
Fernando dos Santos <nando4803@gmail.com>
2022-02-11 17:59:02 +0100

    feat: add home component

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,14 @@
+import { Main } from 'components'
+import { Base } from 'templates'
+
+import * as S from './styles'
+
+const Home = () => (
+  <Base>
+    <S.Wrapper>
+      <Main />
+    </S.Wrapper>
+  </Base>
+)
+
+export default Home
