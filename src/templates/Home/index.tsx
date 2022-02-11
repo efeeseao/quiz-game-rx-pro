c254d38b0c551c12bb5ea906e868767dae4aeecf
Fernando dos Santos <nando4803@gmail.com>
2022-02-11 20:51:18 +0100

    feat: add home template

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,14 +1,30 @@
-import { Main } from 'components'
+import { useState } from 'react'
+
+import { Button, Container, Heading, Modal } from 'components'
 import { Base } from 'templates'
 
 import * as S from './styles'
 
-const Home = () => (
-  <Base>
-    <S.Wrapper>
-      <Main />
-    </S.Wrapper>
-  </Base>
-)
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <Base>
+      <S.Wrapper>
+        <Container>
+          <Heading align="center" color="black">
+            Seja bem-vindo o Jogo de perguntas e respostas
+          </Heading>
+          <S.ButtonWrapper>
+            <Button size="large" onClick={() => setIsOpen(true)}>
+              Começar o Jogo
+            </Button>
+            {isOpen && <Modal setIsOpen={setIsOpen} />}
+          </S.ButtonWrapper>
+        </Container>
+      </S.Wrapper>
+    </Base>
+  )
+}
 
 export default Home
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,3 +1,12 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div``
+export const ButtonWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: ${theme.spacings.medium};
+  `}
+`
