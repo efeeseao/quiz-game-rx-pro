ef606b1258265d9cde1c3eafd24facf893c9a2dc
Fernando dos Santos <nando4803@gmail.com>
2022-02-11 18:00:02 +0100

    feat: add input field component

---
+++ src/components/InputField/index.tsx
@@ -0,0 +1,56 @@
+import { InputHTMLAttributes, useState } from 'react'
+
+import * as S from './styles'
+
+export type TextFieldProps = {
+  error?: string
+  disabled?: boolean
+  icon?: React.ReactNode
+  iconPosition?: 'left' | 'right'
+  initialValue?: string
+  label?: string
+  onInputChange?: (value: string) => void
+} & InputHTMLAttributes<HTMLInputElement>
+
+const TextField = ({
+  error,
+  icon,
+  label,
+  name,
+  onInputChange,
+  disabled = false,
+  iconPosition = 'left',
+  initialValue = '',
+  ...props
+}: TextFieldProps) => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.value
+    setValue(newValue)
+
+    !!onInputChange && onInputChange(newValue)
+  }
+
+  return (
+    <S.Wrapper disabled={disabled} error={!!error}>
+      {!!label && <S.Label htmlFor={name}>{label}</S.Label>}
+      <S.InputWrapper>
+        {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
+        <S.Input
+          type="text"
+          onChange={onChange}
+          value={value}
+          iconPosition={iconPosition}
+          disabled={disabled}
+          name={name}
+          {...(label ? { id: name } : {})}
+          {...props}
+        />
+      </S.InputWrapper>
+      {!!error && <S.Error>{error}</S.Error>}
+    </S.Wrapper>
+  )
+}
+
+export default TextField
---
+++ src/components/InputField/styles.ts
@@ -0,0 +1,102 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+
+import { TextFieldProps } from '.'
+
+type IconPositionProps = Pick<TextFieldProps, 'iconPosition'>
+
+type WrapperProps = Pick<TextFieldProps, 'disabled'> & { error?: boolean }
+
+export const InputWrapper = styled.div`
+  ${({ theme }) => css`
+    align-items: center;
+    background: ${theme.colors.lightGray};
+    border: 0.2rem solid;
+    border-color: ${theme.colors.lightGray};
+    border-radius: 0.2rem;
+    display: flex;
+    padding: 0 ${theme.spacings.xsmall};
+    &:focus-within {
+      box-shadow: 0 0 0.5rem ${theme.colors.primary};
+    }
+  `}
+`
+
+export const Input = styled.input<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    background: transparent;
+    border: 0;
+    color: ${theme.colors.black};
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xsmall} 0;
+    padding-${iconPosition}: ${theme.spacings.xsmall};
+    outline: none;
+    width: ${iconPosition === 'right' ? `calc(100% - 2.2rem)` : `100%`};
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 ${theme.spacings.small}
+        ${theme.colors.lightGray} inset;
+      filter: none;
+      &::first-line {
+        font-family: ${theme.font.family};
+        font-size: ${theme.font.sizes.medium};
+      }
+    }
+  `}
+`
+
+export const Label = styled.label`
+  ${({ theme }) => css`
+    color: ${theme.colors.black};
+    cursor: pointer;
+    font-size: ${theme.font.sizes.small};
+  `}
+`
+
+export const Icon = styled.div<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    color: ${theme.colors.darkGray};
+    display: flex;
+    order: ${iconPosition === 'right' ? 1 : 0};
+    & > svg {
+      height: 100%;
+      width: 2.2rem;
+    }
+  `}
+`
+
+export const Error = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.red};
+    font-size: ${theme.font.sizes.xsmall};
+  `}
+`
+
+const wrapperModifiers = {
+  error: (theme: DefaultTheme) => css`
+    ${InputWrapper} {
+      border-color: ${theme.colors.red};
+    }
+    ${Icon},
+    ${Label} {
+      color: ${theme.colors.red};
+    }
+  `,
+  disabled: (theme: DefaultTheme) => css`
+    ${Label},
+    ${Input},
+    ${Icon} {
+      color: ${theme.colors.gray};
+      cursor: not-allowed;
+      &::placeholder {
+        color: currentColor;
+      }
+    }
+  `
+}
+
+export const Wrapper = styled.div<WrapperProps>`
+  ${({ theme, error, disabled }) => css`
+    ${error && wrapperModifiers.error(theme)}
+    ${disabled && wrapperModifiers.disabled(theme)}
+  `}
+`
