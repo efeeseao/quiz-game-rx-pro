ab03a6da7890c489e04a498283217ac62958235d
Fernando dos Santos <nando4803@gmail.com>
2022-02-11 17:58:08 +0100

    chore: add global style and theme file

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
+    font-size: 1.6rem;
+    line-height: 1.6;
+    background-color: #fafafa;
+    color: #333;
+  }
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,58 @@
+export default {
+  grid: {
+    container: '125rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 400,
+    normal: 600,
+    bold: 700,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+      huge: '5.2rem'
+    }
+  },
+  colors: {
+    primary: '#5052d5',
+    secondary: '#8338ec',
+    mainBg: '#1d1d1d',
+    lightBg: '#F2F2F2',
+    white: '#FAFAFA',
+    black: '#13131a',
+    lightGray: '#EAEAEA',
+    gray: '#E5E5E5',
+    darkGray: '#686C75',
+    subtitle: '#83838e',
+    red: '#FF0000'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  }
+} as const
