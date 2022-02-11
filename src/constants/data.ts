094bacad4e0fabc87c71801a17d1528bf9485ea8
Fernando dos Santos <nando4803@gmail.com>
2022-02-11 21:57:54 +0100

    chore: add data

---
+++ src/constants/data.ts
@@ -0,0 +1,59 @@
+export const data = [
+  {
+    id: '0',
+    question: 'De que cidade são os Beatles?',
+    answer: 'Liverpool',
+    variants: [``, 'Liverpool', 'Manchester', 'Luanda']
+  },
+  {
+    id: '1',
+    question: 'Quantas teclas tem um piano clássico?',
+    answer: '88',
+    variants: [`88`, `90`, `55`]
+  },
+  {
+    id: '2',
+    question: 'De onde vem a Billie Eilish?',
+    answer: 'Los Angeles',
+    variants: [`Los Angeles`, `New York`, `Texas`]
+  },
+
+  {
+    id: '3',
+    question: 'Quem é que inventou a World Wide Web, e quando?',
+    answer: 'Tim Berners-Lee 1990',
+    variants: [`Tim Berners-Lee 1990`, `Steve Jobs 1996`, `Bill Gates 1995`]
+  },
+  {
+    id: '4',
+    question: 'Qual é a capital do Canadá?',
+    answer: 'Ottawa',
+    variants: [`Ottawa`, `Montreal`, `São Paulo`]
+  },
+
+  {
+    id: '5',
+    question: 'Quantos fusos horários tem a Rússia?',
+    answer: '11',
+    variants: [`11`, `5`, `12`]
+  },
+
+  {
+    id: '6',
+    question: 'Que artista famoso de graffiti é de Bristol?',
+    answer: 'Banksy',
+    variants: [`Banksy`, `David Bowie`, `Pablo Picasso`]
+  },
+  {
+    id: '7',
+    question: 'Qual é o país mais pequeno do mundo?',
+    answer: 'Vaticano',
+    variants: [`Vaticano`, `Portugal`, `Chile`]
+  },
+  {
+    id: '8',
+    question: 'Qual era o nome original da marca de roupa Nike?',
+    answer: 'Blue Ribbon Sports',
+    variants: [`Nike`, `Puma`, `Adidas`]
+  }
+]
